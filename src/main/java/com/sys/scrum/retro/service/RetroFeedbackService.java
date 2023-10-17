95c18b111cce5ec67221a1337e6164532e6a2dda
prcsubramani <subbuam73@gmail.com>
2023-10-17 17:26:15 +0530

    Delete src directory

--- src/main/java/com/sys/scrum/retro/service/RetroFeedbackService.java
+++
@@ -1,9 +0,0 @@
-package com.sys.scrum.retro.service;
-
-import com.sys.scrum.retro.model.RetrospectFeedback;
-
-public interface RetroFeedbackService {	
-	RetrospectFeedback createRetroFeedback(RetrospectFeedback comments);	
-	RetrospectFeedback  getRetroFeedbackByIdAndRetrospect(Long id,String retro);
-	
-}
