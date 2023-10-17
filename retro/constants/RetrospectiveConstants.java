87e65a01fdc3becc4473644edd82754430af0fac
prcsubramani <subbuam73@gmail.com>
2023-10-17 17:25:14 +0530

    Delete retro directory

--- retro/constants/RetrospectiveConstants.java
+++
@@ -1,6 +0,0 @@
-package com.sys.scrum.retro.constants;
-
-public class RetrospectiveConstants {	
-  public static final String DATE_FORMAT="dd/MM/yyyy";
-
-}
--- retro/exception/RetrospectDateNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sys.scrum.retro.exception;
-
-public class RetrospectDateNotFoundException extends RuntimeException {
-
-	private static final long serialVersionUID = 3484231469795613153L;
-
-}
